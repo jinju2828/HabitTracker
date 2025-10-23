0fa62945de89adf70808f8861ef9ba5efe5c2825
jinju2828 <jinju2828@gmail.com>
2025-10-22 18:21:05 -0700

    Add HabitForm to App

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,13 +1,15 @@
 import React from 'react';
 import { HabitProgressChart } from './components/HabitProgressChart';
 import HabitChart from './components/HabitChart';
+import { HabitForm } from './components/HabitForm';
 
 function App() {
   return (
     <div style={{ padding: '20px' }}>
       <h1>Habit Tracker Progress</h1>
-      {/* <HabitProgressChart /> */}
+      <HabitForm />
       <HabitChart />
+      <HabitProgressChart />
     </div>
   );
 }
