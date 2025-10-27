6deed7da12d49561a50cf06b9ae999bdfb4d4d55
jinju2828 <jinju2828@gmail.com>
2025-10-27 16:05:52 -0700

    Add Chart Types views

---
+++ frontend/src/components/charts/HeatmapChartView.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+export const HeatmapChartView: React.FC = () => {
+  return (
+    <div
+      style={{
+        padding: '2rem',
+        border: '1px dashed #aaa',
+        borderRadius: '12px',
+        textAlign: 'center',
+        color: '#777',
+      }}
+    >
+      <p>🔥 Heatmap view coming soon...</p>
+      <p>(Like GitHub’s contribution calendar style)</p>
+    </div>
+  );
+};
