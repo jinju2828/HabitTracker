289fd3d4812760c56c430412506d5cace8703314
jinju2828 <jinju2828@gmail.com>
2025-12-16 15:48:44 -0800

    WIP bux fixing - save all doesn't save habit logs for newly created habits

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -28,7 +28,19 @@ function App() {
     const initChecked = async () => {
       const map: Record<number, boolean> = {};
       for (const habit of habits) {
-        const logs = await getHabitLogs(habit.id);
+        // const logs = await getHabitLogs(habit.id);
+
+        let logs: any[] = [];
+
+        try {
+          logs = await getHabitLogs(habit.id);
+        } catch (err: any) {
+          if (err.response?.status !== 404) {
+            throw err; // 진짜 에러만 throw
+          }
+          // 404면 logs = []
+        }
+
         const todayLog = logs.find((l) => {
           const logDate = new Date(l.log_date);
           const logY = logDate.getFullYear();
