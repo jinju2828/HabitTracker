ed6003c9395db9bfcd27c3598e0db43bfc2a7f50
jinju2828 <jinju2828@gmail.com>
2026-01-01 13:23:37 -0800

    WIP - UI, modifying to daily goal

--- frontend/src/components/DailyGoal.tsx
+++ frontend/src/components/DailyGoal.tsx
@@ -1,11 +1,15 @@
 import { useHabits } from '../hooks/useHabits';
 import '../styles/DailyGoal.css';
+import { useAllHabitLogs } from "../hooks/useAllHabitLogs";
 
 export const DailyGoal: React.FC = () => {
     const {habits} = useHabits()
+    const {allLogs} = useAllHabitLogs()
 
+    console.log('DailyGoal habits:', habits.length);
+    console.log('DailyGoal:', allLogs);
     
     return (
-        <div className="daily-goal">Your Daily Goal</div>
+        <div className="daily-goal">Your Daily Goal: Complete 5 habits</div>
     );
 };
