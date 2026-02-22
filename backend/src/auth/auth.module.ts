880d80056341e849a2f6c0a455bcbfbc0838bde1
jinju2828 <jinju2828@gmail.com>
2026-02-22 14:20:21 -0800

    JWT setup

--- backend/src/auth/auth.module.ts
+++ backend/src/auth/auth.module.ts
@@ -1,10 +1,16 @@
-// auth.module.ts
 import { Module } from '@nestjs/common';
-import { AuthController } from './auth.controller';
+import { JwtModule } from '@nestjs/jwt';
 import { AuthService } from './auth.service';
+import { AuthController } from './auth.controller';
 
 @Module({
-  controllers: [AuthController], // ✅ 반드시 등록
+  imports: [
+    JwtModule.register({
+      secret: process.env.JWT_SECRET,
+      signOptions: { expiresIn: '1h' },
+    }),
+  ],
   providers: [AuthService],
+  controllers: [AuthController],
 })
 export class AuthModule {}
