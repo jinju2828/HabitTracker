58fff17eafa7e6dbefee2648066fda8ea1cd4ae3
jinju2828 <jinju2828@gmail.com>
2026-04-06 11:37:44 -0700

    Contact page UI update

--- frontend/src/components/Contact.tsx
+++ frontend/src/components/Contact.tsx
@@ -1,27 +1,30 @@
 import { useNavigate } from "react-router-dom";
+import "../styles/Contact.css";
 
 function Contact() {
   const navigate = useNavigate();
 
   return (
-    <div style={{ padding: 40, textAlign: "center" }}>
-      <h1>Contact</h1>
-      <p>If you have any questions or feedback contact us:</p>
-      <p>Email: jinju2828@gmail.com</p>
-      <p>Github: https://github.com/jinju2828</p>
-      <button 
-        onClick={() => navigate("/")}
-        style={{
-            marginTop: 20,
-            padding: "10px 20px",
-            fontSize: 16,
-            cursor: "pointer",
-            backgroundColor: "#007BFF",
-            color: "#fff",
-            border: "none",
-          }}>
-        ← Back to Habit Tracker
-        </button>
+    <div className="contact-container">
+       <div style={{ padding: 40, textAlign: "center" }}>
+        <h1>Contact</h1>
+        <p>If you have any questions or feedback contact us:</p>
+        <p>Email: jinju2828@gmail.com</p>
+        <p>Github: https://github.com/jinju2828</p>
+        <button 
+          onClick={() => navigate("/")}
+          style={{
+              marginTop: 20,
+              padding: "10px 20px",
+              fontSize: 16,
+              cursor: "pointer",
+              backgroundColor: "#007BFF",
+              color: "#fff",
+              border: "none",
+            }}>
+          ← Back to Habit Tracker
+          </button>
+      </div>
     </div>
   );
 }
